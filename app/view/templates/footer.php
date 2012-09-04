9294f2584729d4cf962cef021d07172f02b6bf4e
FurtherLee <95588lsj@gmail.com>
2012-09-05 01:01:45 +0800

    Add stubs

---
+++ app/view/templates/footer.php
@@ -0,0 +1,9 @@
+</div>
+<div>
+  <!-- js -->  
+</div>
+<footer>
+  <a href="http://github.com/furtherLee">Li Shijian</a>
+</footer>
+</body>
+</html>
---
+++ app/view/templates/header.php
@@ -0,0 +1,10 @@
+<html>
+  <head>
+    <title>Li Shijian</title>
+    <meta charset="utf-8"/>
+  </head>
+  <body id="lee-site">
+    <header>
+
+    </header>
+    <div id="body">
