74fa96bbebb03e93de23bbc68e60e27a71b182d3
FurtherLee <95588lsj@gmail.com>
2012-09-06 00:53:27 +0800

    mie

--- app/pages/about.md
+++ app/pages/about.md
@@ -1,1 +1,38 @@
+Personal Information
+====================
+
+![avatar](http://en.gravatar.com/avatar/a9a1acd402ef13a85a72ee2912848807?s=256")  
+
+Basic Information
+-----------------
+* Full Name: Li Shijian  
+* English Name:  Further Lee  
+* Email: 9588lsj@gmail.com  
+* Living in: Beijing  
+* Current Position: Intern at HP Labs China  
+* CV: Li Shijian.pdf
+
+Education
+----------
+* Undergraduate(2009 ~ now): Shanghai Jiaotong University, ACM Class    
+
+Teaching Assistant
+-------------------
+* 2011 Summer - [PPCA](http://202.120.61.3:8103/wiki/PPCA_2011)  
+* 2011 Fall - Set Theory and Mathematical Logic  
+* 2012 Spring - [Compiler](http://acm.sjtu.edu.cn/compiler/)
+
+Interests
+----------
+* Cloud Computing  
+* Computer Network  
+* Coding  
+* Reading  
+
+Miscellaneous
+--------------
+* [My Blog](http://blog.lishijian.com)
+* [My Github](http://github.com/furtherLee)  
+* [My Bitbucket](https://bitbucket.org/furtherLee)  
+* Social Network: 
 
--- app/view/templates/header.php
+++ app/view/templates/header.php
@@ -1,10 +1,27 @@
 <html>
   <head>
-    <title>Li Shijian</title>
+    <title>Site of Li Shijian</title>
     <meta charset="utf-8"/>
+    <link rel="stylesheet" href="<?php echo CSS_BASE."site.css";?>"/>
   </head>
   <body id="lee-site">
     <header>
-
+      <div id="page-title">
+	<em>Site of Li Shijian</em>
+      </div>
+      <nav>
+	<ul>
+	  <li><a href="<?php echo HOME_BASE."?page=about";?>">About Me</a></li>
+	  <li><a href="<?php echo HOME_BASE."?page=projects";?>">Projects</a></li>
+	  <li><a href="<?php echo HOME_BASE."?page=tools";?>">Tools</a></li>
+	  <li><a href="<?php echo HOME_BASE."?page=books";?>">Books</a></li>
+	</ul>
+      </nav>
     </header>
-    <div id="body">
+      <div class="block block-fork">
+	<a href="https://github.com/furtherLee">
+	  <img style="position: absolute; top: 0; right: 0; border: 0;" src="https://s3.amazonaws.com/github/ribbons/forkme_right_darkblue_121621.png" alt="Fork me on GitHub">
+	</a>
+      </div>
+      <div class="container">
+	<div id="body">
